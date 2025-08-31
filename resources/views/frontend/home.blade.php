aebfabaaac69e34a8d62391db225524310610820
MD. Eftakar Jaman Arfan <141675396+Arfanjaman@users.noreply.github.com>
2025-08-31 16:00:06 +0600

    Implemented Home page of the frontend
    We downloaded the Top News template and implemented it in our web site by sectioning in different sections, a master blade for header and footer and then yielded the home page dynamically

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,28 @@
+@extends('frontend.layouts.master')
+
+@section('content')
+
+<!-- Tranding news  carousel-->
+    @include('frontend.home-components.tranding-news')
+    <!-- End Tranding news carousel -->
+
+    <!-- Hero Slider news -->
+   @include('frontend.home-components.hero-slider')
+    <!-- End Hero Slider news -->
+
+    <div class="large_add_banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="large_add_banner_img">
+                        <img src="images/placeholder_large.jpg" alt="adds">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Main news category -->
+    @include('frontend.home-components.main-news')
+    <!-- End Main news category -->
+@endsection
