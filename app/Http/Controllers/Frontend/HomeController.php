1a4eff6f231df6925ef7e5f1e437e8642e0ae08d
MD. Eftakar Jaman Arfan <141675396+Arfanjaman@users.noreply.github.com>
2025-09-20 00:08:50 +0600

    Meta tags and related posts made dynamic

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -39,8 +39,28 @@ class HomeController extends Controller
             ->activeEntries()->withLocalize()->orderBy('id', 'DESC')->take(4)->get();
 
 
+         $nextPost = News::where('id', '>', $news->id)
+            ->activeEntries()
+            ->withLocalize()
+            ->orderBy('id', 'asc')->first();
 
-       return view('frontend.news-details', compact('news', 'recentNews', 'mostCommonTags'));
+        $previousPost = News::where('id', '<', $news->id)
+            ->activeEntries()
+            ->withLocalize()
+            ->orderBy('id', 'desc')->first();
+
+        $relatedPosts = News::where('slug', '!=', $news->slug)
+            ->where('category_id', $news->category_id)
+            ->activeEntries()
+            ->withLocalize()
+            ->take(5)
+            ->get();
+
+
+
+
+
+      return view('frontend.news-details', compact('news', 'recentNews', 'mostCommonTags', 'nextPost', 'previousPost', 'relatedPosts'));
     }
 
 
