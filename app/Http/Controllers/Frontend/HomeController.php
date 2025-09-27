9ffc285514e33d2bbb518dc54bab548f772a5531
Arfan <soraeshan6@gmail.com>
2025-09-27 19:50:24 +0600

    sidebar made dynamic

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -141,7 +141,11 @@ class HomeController extends Controller
             })->activeEntries()->withLocalize()->paginate(2);
         }
 
-        return view('frontend.news', compact('news'));
+         $recentNews = News::with(['category', 'auther'])
+            ->activeEntries()->withLocalize()->orderBy('id', 'DESC')->take(4)->get();
+        $mostCommonTags = $this->mostCommonTags();
+
+        return view('frontend.news', compact('news', 'recentNews', 'mostCommonTags'));
     }
 
 
